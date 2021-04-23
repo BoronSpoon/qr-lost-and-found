38ce0ee76bd40f7c60785b70971ced8f1559ccf3
rayanti <rayanticlimatic@gmail.com>
2021-04-24 02:08:20 +0900

    qr code not showing up

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -6,57 +6,58 @@
       v-if="!dataReady"
     ></v-progress-circular>
     <v-card class="overflow-hidden" v-if="dataReady">
-      <v-expansion-panels v-model="panels">
-        <v-expansion-panel
+      <v-list>
+        <v-list-group
           v-for="(item, i) in items.slice(shownItem.start,shownItem.end)"
           :key="i"
         >
-          <v-expansion-panel-header class="pa-0">
-            <v-container class="ma-0">
-              <v-row no-gutters>
-                <v-col align="start">
-                  {{item.name}}
-                </v-col>
-                <v-col align="end">
-                  <v-chip
-                    class="mx-1 my-0"
-                    :color="item.lost? 'red': 'grey'"
-                    label
-                    text-color="white"
-                    @click.stop="item.lost = !item.lost"
-                    v-text="item.lost? 'lost': 'not lost'"
-                  >
-                  </v-chip>
-                  <v-chip
-                    class="mx-1 my-0"
-                    :color="item.found? 'red': 'grey'"
-                    label
-                    text-color="white"
-                    v-text="item.found? 'found': 'not found'"
-                  >
-                  </v-chip>
-                </v-col>
-              </v-row>
-            </v-container>            
-            <template v-slot:actions>
-              <v-chip
-                class="mx-1 my-0"
-                color="grey darken-3"
-                label
-                text-color="white"
-              >
-                QR
-                <v-icon>
-                  mdi-chevron-down
-                </v-icon>
-              </v-chip>
-            </template>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <QR :itemName="items[shownItem.start+i].value" :clicked="panels.includes(i)"/>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels> 
+          <v-list-item slot="activator">
+            <v-list-item-title>
+            <v-row no-gutters>
+              <v-col align="start">
+                {{item.name}}
+              </v-col>
+              <v-col align="end">
+                <v-chip
+                  class="mx-1 my-0"
+                  :color="item.lost? 'red': 'grey'"
+                  label
+                  text-color="white"
+                  @click.stop="item.lost = !item.lost"
+                  v-text="item.lost? 'lost': 'not lost'"
+                >
+                </v-chip>
+                <v-chip
+                  class="mx-1 my-0"
+                  :color="item.found? 'red': 'grey'"
+                  label
+                  text-color="white"
+                  v-text="item.found? 'found': 'not found'"
+                >
+                </v-chip>
+              </v-col>
+              <v-col align="end">
+                <v-chip
+                  class="mx-1 my-0"
+                  color="grey darken-3"
+                  label
+                  text-color="white"
+                  @click.stop="item.expand=!item.expand"
+                >
+                  QR
+                  <v-icon>
+                    mdi-chevron-down
+                  </v-icon>
+                </v-chip>
+              </v-col>
+            </v-row>
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <QR :itemName="item.value" :clicked="item.expand"/>
+          </v-list-item>
+        </v-list-group> 
+      </v-list>
       <v-col cols="10">
         <v-pagination
           v-model="currentPage"
@@ -108,10 +109,16 @@ export default {
       // eslint-disable-next-line no-unused-vars
       Object.keys(this.items).map((key, index) => {
         this.items[key].show = true;
+        this.items[key].expand = false;
       });
       this.dataReady = true;
     });
   },
-  mixins: [DatabaseOps]
+  mixins: [DatabaseOps],
+  methods: {
+    s(e){
+      console.log(e)
+    }
+  }
 };
 </script>
