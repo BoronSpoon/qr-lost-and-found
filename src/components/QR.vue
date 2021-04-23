4829ca83d8850b61d48db4b64d9798fdd8e9c1d8
rayanti <rayanticlimatic@gmail.com>
2021-04-23 21:59:27 +0900

    qr image generator moved to external component

---
+++ src/components/QR.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <div id="qrcodeImage"></div>
+    <div id="qrcodeDiv" class="container">
+      <span class="qrcodeText" style="font-weight: bold">StopLosingStuff(Lost＆Found contact) [忘れ物連絡タグ]</span>
+      <span class="qrcodeText">Please contact owner using QR code below</span>
+      <span class="qrcodeText">拾った方はQRコードで所有者へ連絡をお願い致します</span>
+      <span class="qrcodeText" id="qrcodeItemName"></span>
+      <div id="qrcode"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.qrcodeText {
+  line-height: 13px;
+  font-size: 10px;
+  display: block;
+  color: #000000;
+}
+.container {
+  background-color: #ffffff;
+  border-radius: 2px;
+  border-style: solid;
+  border-color: #000000;
+  width: 270px;
+  text-align: center;
+  margin: 0px;
+  padding: 0px;
+  display: none;
+}
+</style>
+
+<script>
+import DatabaseOps from '@/mixins/DatabaseOps'
+
+export default {
+  name: "QR",
+  components: {
+    //
+  },
+  props: {
+    itemName: {
+      type: String,
+      default: ''
+    },
+    clicked: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    //
+  }),
+  watch: {
+    clicked() {
+      if (this.clicked == true){
+        this.$emit('update:clicked', false);
+        if (this.itemName != ''){
+          this.drawQR();
+        }
+      } 
+    }
+  },
+  mixins: [DatabaseOps],
+  methods: {
+    drawQR() {
+      var item = {
+        'currentLocation': '',
+        'foundLocation': '',
+        'found': false,
+        'lost': false,
+        'name': this.itemName
+      }
+      var itemId = this.pushItems(item);
+      this.setItemIds(itemId);
+      var url = 'http://127.0.0.1:5000/?foundItemId='+itemId;
+      var qrcodeImageElementId = 'qrcodeImage';
+      document.getElementById(qrcodeImageElementId).innerHTML = ''; // delete the previous QR code images
+      this.generateQrcode(url, this.itemName);
+      this.divToImage(qrcodeImageElementId);
+    },
+    generateQrcode(url, qrcodeItemName) {
+      var qrcodeItemNameElementId = 'qrcodeItemName';
+      var qrcodeElementId = 'qrcode';
+      document.getElementById(qrcodeItemNameElementId).innerHTML = 'Item identifier: '+qrcodeItemName;
+      document.getElementById(qrcodeElementId).innerHTML = ''; // reset qr code
+      new QRCode(document.getElementById(qrcodeElementId), {
+        text: url,
+        width: 256,
+        height: 256,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+      });
+    },
+    divToImage(qrcodeImageElementId) {
+      var qrcodeDivElementId = 'qrcodeDiv';
+      document.getElementById(qrcodeDivElementId).style.display = 'block'; // show the div for converting to image
+      html2canvas(document.getElementById(qrcodeDivElementId)).then((canvas) => { // convert div(QR code + text) into image
+        document.getElementById(qrcodeImageElementId).appendChild(canvas);
+        document.getElementById(qrcodeDivElementId).style.display = 'none'; // hide the original div
+      });
+    }
+  }
+};
+</script>
+
--- src/views/ItemRegisteration.vue
+++ src/views/ItemRegisteration.vue
@@ -33,50 +33,24 @@
         <v-list-item>
           <v-btn
             class="mr-4"
-            @click="createQR"
+            @click="clicked = true"
           >
             Submit
           </v-btn>         
         </v-list-item>
         <v-spacer/>
         <v-list-item>     
-          <div id="qrcodeImage"></div>
+          <QR :itemName="preferences[0].value" :clicked.sync="clicked"/>
         </v-list-item>
       </v-sheet>
     </v-card>
-    <div id="qrcodeDiv" class="container">
-      <span class="qrcodeText" style="font-weight: bold">StopLosingStuff(Lost＆Found contact) [忘れ物連絡タグ]</span>
-      <span class="qrcodeText">Please contact owner using QR code below</span>
-      <span class="qrcodeText">拾った方はQRコードで所有者へ連絡をお願い致します</span>
-      <span class="qrcodeText" id="qrcodeItemName"></span>
-      <div id="qrcode"></div>
-    </div>
   </div>
 </template>
 
-<style lang="scss">
-.qrcodeText {
-  line-height: 13px;
-  font-size: 10px;
-  display: block;
-  color: #000000;
-}
-.container {
-  background-color: #ffffff;
-  border-radius: 2px;
-  border-style: solid;
-  border-color: #000000;
-  width: 270px;
-  text-align: center;
-  margin: 0px;
-  padding: 0px;
-  display: none;
-}
-</style>
 
 <script>
 import Header from '@/components/Header'
-import DatabaseOps from '@/mixins/DatabaseOps'
+import QR from '@/components/QR'
 
 export default {
   name: "ItemRegisteration",
@@ -84,52 +58,16 @@ export default {
     preferences: [
       { text: 'Name of Item', value: '', type: 'textField'}
     ],
-    title: 'Item Registeration'
+    title: 'Item Registeration',
+    itemName: '',
+    clicked: false,
   }),
   components: {
-    Header
+    Header,
+    QR
   },
   methods: {
-    createQR() {
-      var itemName = this.preferences[0].value;
-      var item = {
-        'currentLocation': '',
-        'foundLocation': '',
-        'found': false,
-        'lost': false,
-        'name': itemName
-      }
-      var itemId = this.pushItems(item);
-      this.setItemIds(itemId);
-      var url = 'http://127.0.0.1:5000/?foundItemId='+itemId;
-      var qrcodeImageElementId = 'qrcodeImage';
-      document.getElementById(qrcodeImageElementId).innerHTML = ''; // delete the previous QR code images
-      this.generateQrcode(url, itemName);
-      this.divToImage(qrcodeImageElementId);
-    },
-    generateQrcode(url, qrcodeItemName) {
-      var qrcodeItemNameElementId = 'qrcodeItemName';
-      var qrcodeElementId = 'qrcode';
-      document.getElementById(qrcodeItemNameElementId).innerHTML = 'Item identifier: '+qrcodeItemName;
-      document.getElementById(qrcodeElementId).innerHTML = ''; // reset qr code
-      new QRCode(document.getElementById(qrcodeElementId), {
-        text: url,
-        width: 256,
-        height: 256,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
-      });
-    },
-    divToImage(qrcodeImageElementId) {
-      var qrcodeDivElementId = 'qrcodeDiv';
-      document.getElementById(qrcodeDivElementId).style.display = 'block'; // show the div for converting to image
-      html2canvas(document.getElementById(qrcodeDivElementId)).then((canvas) => { // convert div(QR code + text) into image
-        document.getElementById(qrcodeImageElementId).appendChild(canvas);
-        document.getElementById(qrcodeDivElementId).style.display = 'none'; // hide the original div
-      });
-    }
-  },
-  mixins: [DatabaseOps]
+    //
+  }
 };
 </script>
