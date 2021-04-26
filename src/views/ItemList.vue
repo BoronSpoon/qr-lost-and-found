98055212bf59819e53425ef4f554b7bceba84949
rayanti <rayanticlimatic@gmail.com>
2021-04-26 22:17:34 +0900

    fixed page change behavior, css

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -5,45 +5,54 @@
       indeterminate
       v-if="!dataReady"
     ></v-progress-circular>
-    <v-card class="overflow-hidden" v-if="dataReady">
+    <v-card class='overflow-hidden' v-if="dataReady">
       <v-list>
         <v-list-item
           v-for="(item, i) in items.slice(shownItem.start,shownItem.end)"
           :key="i"
         >
           <v-list-item-content>
-            <v-row>
-              <v-col align="start">
+            <v-row class='align-start'>
+              <v-col
+                cols='auto'
+                class='mr-auto px-3 py-2'  
+              >
                 {{item.name}}
               </v-col>
-              <v-col align="end">
+              <v-spacer/>
+              <v-col
+                cols='auto'
+                class='ml-auto pa-1'
+              >
                 <v-chip
-                  class="mx-1 my-0"
+                  class='mx-1 my-0'
                   :color="item.lost? 'red': 'grey'"
                   label
-                  text-color="white"
+                  text-color='white'
                   @click.stop="item.lost = !item.lost"
                   v-text="item.lost? 'lost': 'not lost'"
                 >
                 </v-chip>
                 <v-chip
-                  class="mx-1 my-0"
+                  class='mx-1 my-0'
                   :color="item.found? 'red': 'grey'"
                   label
-                  text-color="white"
+                  text-color='white'
                   v-text="item.found? 'found': 'not found'"
                 >
                 </v-chip>
               </v-col>
-              <v-col align="end">
+              <v-col
+                cols='auto'
+                class='ml-auto pa-1'
+              >
                 <v-chip
-                  class="mx-1 my-0"
-                  color="grey darken-3"
+                  class='mx-1 my-0'
+                  color='grey darken-3'
                   label
-                  text-color="white"
+                  text-color='white'
                   @click.stop="changeExpandState(i)"
                 >
-                  {{item.expand}}
                   QR
                   <v-icon>
                     mdi-chevron-down
@@ -51,8 +60,8 @@
                 </v-chip>
               </v-col>
               <v-col 
-                cols="12" 
-                class="pa-0 text-center"
+                cols='12'
+                class='pa-1 text-center'
               >
                 <div v-show="item.expand">
                   <QR :itemName="item.name" :clicked="item.expand" :itemId="item.id"/>
@@ -62,13 +71,14 @@
           </v-list-item-content>
         </v-list-item> 
       </v-list>
-      <v-col cols="10">
-        <v-pagination
-          v-model="currentPage"
-          class="my-4"
-          :length="totalPages"
-        ></v-pagination>
-      </v-col>
+      <v-row no-gutters>
+        <v-col cols='12'>
+          <v-pagination
+            v-model="currentPage"
+            class='my-4'
+            :length="totalPages"
+          ></v-pagination>
+        </v-col>
     </v-card>
   </div>
 </template>
@@ -79,7 +89,7 @@ import QR from '@/components/QR'
 import DatabaseOps from '@/mixins/DatabaseOps'
 
 export default {
-  name: "ItemList",
+  name: 'ItemList',
   data: () => ({
     items: [],
     currentPage: 1,
@@ -100,6 +110,7 @@ export default {
         start: (this.currentPage-1)*this.itemsPerPage, // start item no for current page
         end: Math.min((this.currentPage)*this.itemsPerPage, this.items.length) // end item no for current page
       };
+      this.changeExpandState(-1); // set expand to false for all list
     }
   },
   mounted() {
