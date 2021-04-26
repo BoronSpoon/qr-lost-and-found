a584c25c301b981f3f7a361a04d30214d5425d6d
rayanti <rayanticlimatic@gmail.com>
2021-04-26 23:35:18 +0900

    hover action fixed

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -7,69 +7,77 @@
     ></v-progress-circular>
     <v-card class='overflow-hidden' v-if="dataReady">
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items.slice(shownItem.start,shownItem.end)"
-          :key="i"
+        <v-list-item-group
+          v-model="selectedItem"
+          color='primary'
         >
-          <v-list-item-content>
-            <v-row class='align-start'>
-              <v-col
-                cols='auto'
-                class='mr-auto px-3 py-2'  
-              >
-                {{item.name}}
-              </v-col>
-              <v-spacer/>
-              <v-col
-                cols='auto'
-                class='ml-auto pa-1'
-              >
-                <v-chip
-                  class='mx-1 my-0'
-                  :color="item.lost? 'red': 'grey'"
-                  label
-                  text-color='white'
-                  @click.stop="item.lost = !item.lost"
-                  v-text="item.lost? 'lost': 'not lost'"
+          <v-list-item
+            v-for="(item, i) in items.slice(shownItem.start,shownItem.end)"
+            :key="i"
+          >
+            <v-list-item-content>
+              <v-row class='align-start'>
+                <v-col
+                  cols='auto'
+                  class='mr-auto px-3 py-2'  
                 >
-                </v-chip>
-                <v-chip
-                  class='mx-1 my-0'
-                  :color="item.found? 'red': 'grey'"
-                  label
-                  text-color='white'
-                  v-text="item.found? 'found': 'not found'"
+                  {{item.name}}
+                </v-col>
+                <v-spacer/>
+                <v-col
+                  cols='auto'
+                  class='ml-auto pa-1'
                 >
-                </v-chip>
-              </v-col>
-              <v-col
-                cols='auto'
-                class='ml-auto pa-1'
-              >
-                <v-chip
-                  class='mx-1 my-0'
-                  color='grey darken-3'
-                  label
-                  text-color='white'
-                  @click.stop="changeExpandState(i)"
+                  <v-chip
+                    class='mx-1 my-0'
+                    :color="item.lost? colors[5]: colors[1]"
+                    label
+                    text-color='white'
+                    @click.stop="item.lost = !item.lost"
+                    v-text="item.lost? 'lost': 'not lost'"
+                  >
+                  </v-chip>
+                  <v-chip
+                    class='mx-1 my-0'
+                    :color="item.found? colors[5]: colors[1]"
+                    label
+                    text-color='white'
+                    v-text="item.found? 'found': 'not found'"
+                  >
+                  </v-chip>
+                </v-col>
+                <v-col
+                  cols='auto'
+                  class='ml-auto pa-1'
                 >
-                  QR
-                  <v-icon>
-                    mdi-chevron-down
-                  </v-icon>
-                </v-chip>
-              </v-col>
-              <v-col 
-                cols='12'
-                class='pa-1 text-center'
-              >
-                <div v-show="item.expand">
-                  <QR :itemName="item.name" :clicked="item.expand" :itemId="item.id"/>
-                </div>
-              </v-col>
-            </v-row>
-          </v-list-item-content>
-        </v-list-item> 
+                  <v-chip
+                    class='mx-1 my-0'
+                    :color="colors[3]"
+                    label
+                    text-color='white'
+                    @click.stop="changeExpandState(i)"
+                  >
+                    QR
+                    <v-icon>
+                      mdi-chevron-down
+                    </v-icon>
+                  </v-chip>
+                </v-col>
+                <v-col 
+                  cols='12'
+                  class='pa-1 text-center'
+                >
+                  <v-container
+                    class='fluid fill-height'
+                    v-show="item.expand"
+                  >
+                    <QR :itemName="item.name" :clicked="item.expand" :itemId="item.id"/>
+                  </v-container>
+                </v-col>
+              </v-row>
+            </v-list-item-content>
+          </v-list-item> 
+        </v-list-item-group>
       </v-list>
       <v-row no-gutters>
         <v-col cols='12'>
@@ -96,21 +104,32 @@ export default {
     itemsPerPage: 10,
     totalPages: 0,
     shownItem: {},
+    selectedItem: '',
     title: 'Item List',
     dataReady: false,
     panels: [],
+    colors: ['gray', 'grey darken-1', 'grey darken-2', 'grey darken-3', 'black', 'red']
   }),
   components: {
     Header,
     QR
   },
   watch: {
+    selectedItem () { 
+      // when item different from expanded one is clicked
+      if ((typeof this.selectedItem !== 'undefined') && (this.selectedItem !== -1)){ // if none is clicked, selectedItem is undefined or -1
+        if (this.items[this.shownItem.start+this.selectedItem].expand === false){
+          this.changeExpandState(-1); // set expand to false for all list
+        };
+      } 
+    },
     currentPage(){
       this.shownItem = {
         start: (this.currentPage-1)*this.itemsPerPage, // start item no for current page
         end: Math.min((this.currentPage)*this.itemsPerPage, this.items.length) // end item no for current page
       };
       this.changeExpandState(-1); // set expand to false for all list
+      this.selectedItem = '';
     }
   },
   mounted() {
@@ -131,6 +150,7 @@ export default {
   mixins: [DatabaseOps],
   methods: {
     changeExpandState(index){ // expand qr for clicked list item, close for others
+      this.selectedItem = index; // enable hover color on expanded component
       for (var i = 0; i < this.itemsPerPage; i++){
         var item = this.items[this.shownItem.start+i];
         if (i == index){ // expand
